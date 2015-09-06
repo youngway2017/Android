83582b89f83a9c1e1a217c1688c7db0cce7adf59
yangwei <654183746@qq.com>
2015-09-06 18:32:43 +0800

    Android Studio with Git test

--- GitTest/app/src/main/java/yw/cn/gittest/MainActivity.java
+++ GitTest/app/src/main/java/yw/cn/gittest/MainActivity.java
@@ -4,13 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
     @Override
